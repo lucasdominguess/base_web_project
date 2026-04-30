afe2e99499cf8588904369e4ab17df667f418b7f
lucas <lucasdominguesofficial@gmail.com>
2026-04-30 18:20:25 -0300

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        // web: __DIR__.'/../routes/web.php',
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
@@ -96,8 +96,9 @@ return Application::configure(basePath: dirname(__DIR__))
             ];
 
             if (config('app.debug')) {
-                $response['error'] = $exception->getMessage();
+                $response['error'] = mb_convert_encoding($exception->getMessage(), 'UTF-8', 'auto');
                 $response['exception'] = class_basename($exception);
+                $response['trace'] = $exception->getTraceAsString();
             }
 
             return response()->json($response, $status);
